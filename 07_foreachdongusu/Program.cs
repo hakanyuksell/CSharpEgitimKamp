6178003c68e1142e8e647a4b454dfbe122825a66
Hakan <120856145+hakanyuksell@users.noreply.github.com>
2024-10-23 16:26:54 +0300

    Add files via upload

---
+++ 07_foreachdongusu/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_foreachdongusu
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Foreach Döngüsü
+
+			//Foreach(1;2;3;4)
+
+			//1: Değişken Türü
+			//2: Değişken Adı 
+			//3: In
+			//4: Liste, Koleksiyon, Dizi
+
+			//string[] cities = { "Milano", "roma", "budapeşte", "Ankara" };
+
+			//foreach (string x in cities)
+			//{
+			//	Console.WriteLine(x);
+			//}
+
+			//******************************************************************
+
+			//int[] numbers = { 23, 4, 343443, 23, 67, 45, 98, 56, 54 };
+			//foreach (int i in numbers)
+			//{
+			//	Console.WriteLine(i);
+			//}
+
+			//**********İkiye tam Bölünen Sayılar***********
+
+
+			//int[] numbers = { 23, 4, 343443, 23, 67, 45, 98, 56, 54 };
+			//foreach (int number in numbers)
+			//{
+			//	if(number%2 == 0)
+			//	{
+			//		Console.WriteLine(number);
+			//	}
+			//}
+
+			//******************************************************************
+
+			//List <int> numbers = new List<int>()
+			//{
+			//	1,2,3,4,5,6	
+			//};
+
+			//foreach (int i in numbers)
+			//{
+			//	Console.WriteLine(i);
+			//}
+			//Foreach döngüsü sadece dizilerde değil listelerde
+			//de kullanılmaktadır. 
+
+
+			//******************************************************************
+
+			//string word = "MERHABA";
+			//foreach (var c in word)
+			//{
+			//	Console.WriteLine(c);
+			//}
+
+			//******************************************************************
+
+			#endregion
+
+			#region Örnek Sınav Sistemi Uygulaması
+
+			Console.WriteLine("*****C# EĞİTİM KAMPI SINAV UYGULAMASI*****");
+			Console.WriteLine();
+
+			//Sınıftaki Öğrenci Sayısını Kullanıcıdan Alma 
+			Console.WriteLine("------------------------------------");
+			Console.Write("Sınıfınızda kaç öğrenci var?: ");
+			int studentCount = int.Parse(Console.ReadLine());
+			Console.WriteLine("------------------------------------");
+
+			//Öğrenci isimleri ve not ortalamalarını saklayacak diziler
+			string[] studentNames = new string[studentCount];
+			double[] studentExamAvg = new double[studentCount];
+			
+			for (int i = 0; i < studentCount; i++)
+			{
+				Console.Write($"{i + 1}. öğrencinin ismini giriniz: ");
+				studentNames[i] = Console.ReadLine();
+			
+				double totalExam = 0;
+				//her öğrenci için 3 sınav notu girişi
+				for (int j = 0; j < 3; j++)
+				{
+					Console.Write($"{studentNames[i]} adlı öğrencinin {j + 1} sınav notunu giriniz: ");
+					double value = double.Parse(Console.ReadLine());
+					totalExam += value; //notları topluyoruz
+					studentExamAvg[i] += totalExam / 3;
+				}
+				Console.WriteLine();
+			}
+			//Öğrencilerin ortalamaları ve geçip kalma durumları
+			for (int i = 0; i < studentCount; i++)
+			{
+				Console.WriteLine($"{studentNames[i]}  adlı öğrencinin ortalaması  {studentExamAvg[i]}");
+				if (studentExamAvg[i] >= 50)
+				{
+					Console.WriteLine($"{studentNames[i]} adlı öğrenci dersi geçti");
+				}
+				else
+				{
+					Console.WriteLine($"{studentNames[i]} adlı öğrenci dersten kaldı");
+				}
+			}
+			#endregion
+			Console.Read();
+		}
+	}
+}
---
+++ 07_foreachdongusu/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("07_foreachdongusu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("07_foreachdongusu")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8224d704-4150-4610-a209-94fab912cbe2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
