717d95ed2a72702baf9f4498ff2d34b3c1a6469a
Hakan <120856145+hakanyuksell@users.noreply.github.com>
2024-10-23 16:00:09 +0300

    01_Main Subject

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region YazdırmaKomutları
+			//Console.WriteLine("**Yemek Kategorileri**");
+			//Console.WriteLine();
+			//Console.WriteLine("1- Çorbalar");
+			//Console.WriteLine("2- Ana Yemekler");
+			//Console.WriteLine("3- Soğuk Başlangıçlar");
+			//Console.WriteLine("4- Tatlılar");
+			//Console.Read();
+			#endregion
+
+			#region String Değişkenler
+
+			//String 
+			//Değişken_türü değişken_Adı ;
+
+			//string name, surname, age;
+			//name = "Hakan";
+			//surname = "Yüksel";
+			//age = "23";
+
+			//Console.WriteLine("***REZERVASYON KARTI***");
+			//Console.WriteLine("-------------------------");
+			//Console.WriteLine("iSİM: " + name);
+			//Console.WriteLine("SOYİSİM: " + surname);
+			//Console.WriteLine("YAŞ: " + age);
+
+
+			//Console.Read();
+
+			#endregion
+
+			#region int Değişkenler
+
+			//int = tam sayı türündeki değişkenlerdir.  
+
+			//int hamburgerprice = 500;
+			//int cokeprice = 35;
+			//int waterprice = 10;
+			//int friesprice = 50;
+			//int pizzaprice = 250;
+			//int lemonadeprice = 30;
+
+			//Console.WriteLine("******Restoran Menü Fiyatı******");
+			//Console.WriteLine();
+			//Console.WriteLine("-------Hamburger: " + hamburgerprice + " TL");
+			//Console.WriteLine("-------Pizza: " + pizzaprice + " TL");
+			//Console.WriteLine("-------Kola: " + cokeprice + " TL");
+			//Console.WriteLine("-------Limonata: " + lemonadeprice + " TL");
+			//Console.WriteLine("-------Su: " + waterprice + " TL");
+			//Console.WriteLine();
+		
+			//int total = hamburgerprice + cokeprice + waterprice + friesprice + pizzaprice + lemonadeprice;
+			//Console.WriteLine("toplam = " + total);
+
+			//Console.ReadLine();
+			#endregion
+
+		
+
+		}
+	}
+}
